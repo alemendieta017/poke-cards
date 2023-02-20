44069be59d8830d44a5c5e4330f749e44d5a58a3
Alejandro Mendieta <66228099+alemendieta017@users.noreply.github.com>
2023-02-20 21:56:14 +0000

    dev pagination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,6 +1,25 @@
-const Pagination = () => {
+import styles from './pagination.module.css'
+
+const Pagination = ({currentPage = 2, totalPages = 30, buttonLinks}) => {
   return (
-    <p>Pagination</p>
+    <>
+    {currentPage >= 4 && (
+      <button>principio</button>
+    )}
+    
+    <button className={currentPage === 1 && styles.current}>1</button>
+    {(currentPage <= 3 || currentPage >= totalPages-2) && <button className={currentPage === 2 && styles.current}>2</button>}
+    {currentPage >= 4 && <button>...</button>}
+    {(currentPage <= 3 || currentPage >= totalPages-2) && <button className={currentPage === 3 && styles.current}>3</button>}
+
+    {(currentPage <= 4 || currentPage >= totalPages - 2) && <button>...</button>}
+
+    {currentPage < 4 && <button>{totalPages - 2}</button>}
+    {currentPage < 4 && <button>{totalPages - 1}</button>}
+    {currentPage < 4 && <button>{totalPages}</button>}
+
+    {currentPage !== totalPages && <button>final</button>}
+    </>
   )
 }
 
